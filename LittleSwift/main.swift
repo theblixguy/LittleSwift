05efa497d32c0de078dd0988bedd16ae7ed28ca8
Suyash Srijan <suyashsrijan@outlook.com>
2018-10-16 20:13:07 +0100

    Initial Commit

---
+++ LittleSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LittleSwift
+//
+//  Created by Suyash Srijan on 16/10/2018.
+//  Copyright © 2018 Suyash Srijan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
